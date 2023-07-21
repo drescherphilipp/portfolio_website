3107da2fbe87a600004cf2a1dab5e22a48d40611
Philipp Drescher <d.phil@gmx.at>
2023-07-21 12:02:43 +0200

    re added typewriter animation

--- scripts/skillBarAnimation.js
+++ scripts/skillBarAnimation.js
@@ -1,18 +1,33 @@
 $(function () { // wait for document ready
-    var trigger_pos = 0.8;
     var controller = new ScrollMagic.Controller();
 
     // header_skills
-/*
-    var typewriter_skills = new Typewriter(document.getElementById('header_skills'), { loop: false });
-    var scene_skills = new ScrollMagic.Scene({triggerElement: '#header_skills', triggerHook: 1})
+    var typewriter_portfolio = new Typewriter(document.getElementById('header_portfolio'), { loop: false, autoStart: true, cursor: "■"});
+    var typewriter_skills = new Typewriter(document.getElementById('header_skills'), { loop: false, autoStart: true, cursor: "■"});
+    var typewriter_contact = new Typewriter(document.getElementById('header_contact'), { loop: false, autoStart: true, cursor: "■"});
+
+
+    var scene_portfolio = new ScrollMagic.Scene({triggerElement: '#header_portfolio', triggerHook: 1})
     .on("enter", function (event) {
-      typewriter_skills.typeString('Skills').start();
-    })
-    .on("leave", function(event) {
-      typewriter_skills.stop();
+      typewriter_portfolio.deleteAll();
+      typewriter_portfolio.typeString('Portfolio');
     })
     .addTo(controller);
-*/
+
+    var scene_skills = new ScrollMagic.Scene({triggerElement: '#header_skills', triggerHook: 1})
+    .on("enter", function (event) {
+      typewriter_skills.deleteAll();
+      typewriter_skills.typeString('Skills');
+    })
+    .addTo(controller);
+
+
+    var scene_contact = new ScrollMagic.Scene({triggerElement: '#header_contact', triggerHook: 1})
+    .on("enter", function (event) {
+      typewriter_contact.deleteAll();
+      typewriter_contact.typeString('Contact');
+    })
+    .addTo(controller);
+
 
 });
