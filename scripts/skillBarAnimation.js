521241b0edca1731b97fbc413ce538fc70bf4c93
Philipp Drescher <d.phil@gmx.at>
2023-07-22 11:07:05 +0200

    added skill bar animations

--- scripts/skillBarAnimation.js
+++ scripts/skillBarAnimation.js
@@ -1,5 +1,52 @@
 $(function () { // wait for document ready
+    var trigger_pos = 0.7;
+    var animationDuration = 2;
+    var controller = new ScrollMagic.Controller();
 
+    // PROGRAMMING
+    new ScrollMagic.Scene({triggerElement: '#header_programming', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_c', animationDuration, { width: '0%' }, { width: '50%' }))
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: '#header_programming', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_cpp', animationDuration, { width: '0%' }, { width: '60%' }))
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: '#header_programming', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_cs', animationDuration, { width: '0%' }, { width: '80%' }))
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: '#header_programming', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_python', animationDuration, { width: '0%' }, { width: '90%' }))
+    .addTo(controller);
+
+
+    // ENGINES
+    new ScrollMagic.Scene({triggerElement: '#header_engines', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_unreal', animationDuration, { width: '0%' }, { width: '15%' }))
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: '#header_engines', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_unity', animationDuration, { width: '0%' }, { width: '80%' }))
+    .addTo(controller);
+
+
+    // MISC
+    new ScrollMagic.Scene({triggerElement: '#header_misc', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_fmod', animationDuration, { width: '0%' }, { width: '70%' }))
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: '#header_misc', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_ableton', animationDuration, { width: '0%' }, { width: '90%' }))
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: '#header_misc', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_blender', animationDuration, { width: '0%' }, { width: '40%' }))
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: '#header_misc', triggerHook: trigger_pos})
+    .setTween(TweenMax.fromTo('#skill_photoshop', animationDuration, { width: '0%' }, { width: '60%' }))
+    .addTo(controller);
 
 
 });
--- scripts/typewriterBind.js
+++ scripts/typewriterBind.js
@@ -1,56 +1,5 @@
   $(function () { // wait for document ready
-
-		// Initialize ScrollMagic controller
-
-
-
-
-
-
-
-
     var controller = new ScrollMagic.Controller();
-
-
-		// Select the pinned <div> element using its id "gameboy"
-
-		// Create a ScrollMagic scene to pin the <div> for 100vh
-/*
-    var tween = TweenMax.fromTo('#cartridge',
-      10, // Duration of the animation in seconds
-      { y: "-100%" }, // From: starting values
-      { y: "0%" } // To: ending values
-    );    
-
-		new ScrollMagic.Scene({
-			triggerElement: '#cartridge',
-			duration: "100%", // Pin the element for 100vh units of scrolling
-			triggerHook: "onLeave", // Pin when the top of the <div> reaches the top of the viewport
-		})
-    .setTween(tween)
-		.setPin('#cartridge')
-		.addTo(controller);
-
-
-    var scaleTween = TweenMax.fromTo(gameboy,
-      1, // Duration of the animation in seconds
-      { scale: 1, delay: "50vh" }, // From: starting values with 50vh delay
-      { scale: 20 } // To: ending values
-    );
-
-
-    new ScrollMagic.Scene({
-			triggerElement: '#gameboy',
-			duration: "100%", // Pin the element for 100vh units of scrolling
-			triggerHook: "onLeave", // Pin when the top of the <div> reaches the top of the viewport
-		})
-    .setTween(scaleTween)
-		.setPin('#gameboy')
-		.addTo(controller);
-
-*/
-
-
 
 
     var typewriter_portfolio = new Typewriter(document.getElementById('header_portfolio'), { loop: false, autoStart: true, cursor: "■"});
@@ -58,14 +7,14 @@
     var typewriter_contact = new Typewriter(document.getElementById('header_contact'), { loop: false, autoStart: true, cursor: "■"});
 
 
-    var scene_portfolio = new ScrollMagic.Scene({triggerElement: '#header_portfolio', triggerHook: 1})
+    new ScrollMagic.Scene({triggerElement: '#header_portfolio', triggerHook: 1})
     .on("enter", function (event) {
       typewriter_portfolio.deleteAll();
       typewriter_portfolio.typeString('Portfolio');
     })
     .addTo(controller);
 
-    var scene_skills = new ScrollMagic.Scene({triggerElement: '#header_skills', triggerHook: 1})
+    new ScrollMagic.Scene({triggerElement: '#header_skills', triggerHook: 1})
     .on("enter", function (event) {
       typewriter_skills.deleteAll();
       typewriter_skills.typeString('Skills');
@@ -73,7 +22,7 @@
     .addTo(controller);
 
 
-    var scene_contact = new ScrollMagic.Scene({triggerElement: '#header_contact', triggerHook: 1})
+    new ScrollMagic.Scene({triggerElement: '#header_contact', triggerHook: 1})
     .on("enter", function (event) {
       typewriter_contact.deleteAll();
       typewriter_contact.typeString('Contact');
